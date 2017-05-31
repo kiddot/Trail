88941048a62209975f73acaa2f8c2f87f2386137
kiddot <kiddo1024@163.com>
2017-05-31 20:25:56 +0800

    add server config

---
+++ server/src/main/java/net/server/im/event/MessageQoSEventListenerS2C.java
@@ -0,0 +1,15 @@
+package net.server.im.event;
+
+import net.server.im.protocal.Protocol;
+
+import java.util.ArrayList;
+
+/**
+ * Created by kiddo on 17-5-31.
+ */
+
+public abstract interface MessageQoSEventListenerS2C {
+    public abstract void messagesLost(ArrayList<Protocol> paramArrayList);
+
+    public abstract void messagesBeReceived(String paramString);
+}
